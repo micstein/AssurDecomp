71501b5701c6b44c9932f26f51b19c0151336ed8
Michael Rothstein <42815678+micstein@users.noreply.github.com>
2020-04-19 15:01:09 -0400

    Update sym-assur.cpp

--- sym-assur.cpp
+++ sym-assur.cpp
@@ -10,15 +10,12 @@ int ycenter = 0;
 bool xsym = false;
 bool ysym = false;
 //Ex 1: Symmetric along x, center at x = 4
-//int vertLoc[10][2] = {{1,0}, {7,0}, {1,2}, {3,3}, {1,5}, {7,2}, {5,3}, {7,5}, {3,6}, {5,6}};
+int vertLoc[10][2] = {{1,0}, {7,0}, {1,2}, {3,3}, {1,5}, {7,2}, {5,3}, {7,5}, {3,6}, {5,6}};
 //Ex 2 Y symmetry along y = 3
-int vertLoc[10][2] = {{0,0}, {0,6}, {2,2}, {4,2}, {3,0}, {2,4}, {4,4}, {3,6}, {5,6}, {5,0}};
+//int vertLoc[10][2] = {{0,0}, {0,6}, {2,2}, {4,2}, {3,0}, {2,4}, {4,4}, {3,6}, {5,6}, {5,0}};
+//Ex 3 X and Y Sym
+//int vertLoc[14][2] = {{1,0}, {7,0}, {1,6}, {7,6}, {0,2}, {0,4}, {3,3}, {5,3}, {8,2}, {8,4}, {2,1}, {2,5}, {6,5}, {6,1}};
 
-
-//Ex 1: Symmetric graph with two SCCs
-//int vertLoc[6][2] = {{1,1}, {2,2}, {4,2}, {5,1}, {1,3}, {5,3}};
-//Ex 2: Symmetric graph with extra verts that get separated
-//int vertLoc[8][2] = {{1,1}, {2,2}, {4,2}, {5,1}, {1,3}, {5,3}, {2,4}, {4,4}};
 int numPinned;
 int numVerticies;
 int totalVert;
@@ -32,11 +29,11 @@ class Graph
     list<int> *adj;
 
     // A Recursive DFS based function used by Assur()
-    void FindSCC(int nextVert, int disc[], int low[], stack<int> *st, bool stackMember[]);
+    void FindSCC(int nextVert, int disc[], int low[], stack<int> *graphStack, bool stackMember[]);
 
 public:
     Graph(int numVert, int pinned);
-    void addEdge(int v, int w);
+    void addEdge(int from, int to);
     void Assur();
 };
 
@@ -48,20 +45,20 @@ Graph::Graph(int numVert, int pinned)
 }
 
 //add edge to graph
-void Graph::addEdge(int v, int w)
+void Graph::addEdge(int from, int to)
 {
-    adj[v].push_back(w);
+    adj[from].push_back(to);
 }
 
 // finds and prints strongly connected components using DFS traversal
 //u = next vertex, disc = discovery times of verticies, low = earliest visited
-//st = connections, stackmember = check if node in a stack, sym = is the graph symmetrical
-void Graph::FindSCC(int nextVert, int disc[], int low[], stack<int> *st, bool stackMember[])
+//graphStack = stack, stackmember = check if node in a stack
+void Graph::FindSCC(int nextVert, int disc[], int low[], stack<int> *graphStack, bool stackMember[])
 {
     // Initialize
     static int time = 0;
     disc[nextVert] = low[nextVert] = ++time;
-    st->push(nextVert);
+    graphStack->push(nextVert);
     stackMember[nextVert] = true;
 
     // traverse adjacent vertices
@@ -71,10 +68,26 @@ void Graph::FindSCC(int nextVert, int disc[], int low[], stack<int> *st, bool st
         //current adjacent
         int v = *i;
 
+        //no need to decompose both ways
+        if (xsym && ysym) {
+            ysym = false;
+        }
+
+        //Essence of Sym-Adapted Assur. Only necessary to do half the decomp and then reflect it across center
+        if (xsym) {
+             if ((vertLoc[nextVert][0]) > center) {
+                 continue;
+             }
+        } else if (ysym) {
+            if ((vertLoc[nextVert][1]) > ycenter) {
+                continue;
+            }
+        }
+
         // recursion if current adjacent hasnt been visited
         if (disc[v] == -1)
         {
-            FindSCC(v, disc, low, st, stackMember);
+            FindSCC(v, disc, low, graphStack, stackMember);
             // Tarjan Case 1: Check if  subtree with 'v' has a connection to one of the ancestors of 'u'
             low[nextVert] = min(low[nextVert], low[v]);
         }
@@ -90,9 +103,9 @@ void Graph::FindSCC(int nextVert, int disc[], int low[], stack<int> *st, bool st
     int checker[4] = {0,0,0,0};
     if (low[nextVert] == disc[nextVert])
     {
-        while (st->top() != nextVert)
+        while (graphStack->top() != nextVert)
         {
-            w = (int) st->top();
+            w = (int) graphStack->top();
             if ((w < numPinned) && (xsym)){
                 //case 0: x reflected assur
                 if (center < vertLoc[w][0])
@@ -111,43 +124,39 @@ void Graph::FindSCC(int nextVert, int disc[], int low[], stack<int> *st, bool st
             }
             cout << w << " ";
             stackMember[w] = false;
-            st->pop();
+            graphStack->pop();
             stackCount++;
         }
-        w = (int) st->top();
-        cout << w;
+        w = (int) graphStack->top();
+
+        if (xsym) {
+            if ((vertLoc[w][0]) < center) {
+                cout << w;
+            }
+        } else if (ysym) {
+            if ((vertLoc[w][1]) < ycenter) {
+                cout << w;
+            }
+        }
         //Label the decompositions
-        if (((w<numPinned) && (center < vertLoc[w][0]) && (xsym)) || checker[0] ){
-                cout << " <-- Assur Reflection across X = " << center << "\n";
-        }
-        if (((w<numPinned) && (center < vertLoc[w][1]) && (ysym))  || checker[2] ){
-                cout << " <-- Assur Reflection across Y = " << ycenter << "\n";
-        }
         if (((w<numPinned) && (center > vertLoc[w][0]) && (xsym)) || checker[1] ){
-                    cout << " <-- Assur Decomp of left side across X = " << center << "\n";
+                    cout << " <-- Assur Decomp of left side with an equivalent graph reflected across X = " << center << "\n";
         }
         if (((w<numPinned) && (center > vertLoc[w][1]) && (ysym)) || checker[3] ){
-            cout << " <-- Assur Decomp of bottom below Y = " << ycenter << "\n";
+            cout << " <-- Assur Decomp of bottom with an equivalent graph reflected across Y = " << ycenter << "\n";
         }
         if ((stackCount < 2) && (vertLoc[w][0] < center) && xsym){
-            cout << " <-- Isostatic graph beneath X = " << center << "\n";
+            cout << " <-- Isostatic graph beneath X = " << center << " with an equivalent graph across it\n";
         }
         if ((stackCount < 2) && (vertLoc[w][1] < ycenter) && ysym){
-            cout << " <-- Isostatic graph beneath Y = " << ycenter << "\n";
-        }
-        if ((stackCount < 2) && (vertLoc[w][0] > center) && xsym){
-            cout << " <-- Reflected Isostatic graph\n";
-        }
-        if ((stackCount < 2) && (vertLoc[w][1] > ycenter) && ysym){
-            cout << " <-- Reflected Isostatic graph\n";
+            cout << " <-- Isostatic graph beneath Y = " << ycenter << " with an equivalent graph across it\n";
         }
 
         stackCount = 0;
 
 
-
         stackMember[w] = false;
-        st->pop();
+        graphStack->pop();
     }
 }
 
@@ -174,7 +183,7 @@ void Graph::Assur()
 }
 
 
-bool isXSymmetric(int total, int coords[][2], int edges[][10]) {
+bool isXSymmetric(int total, int coords[][2]) {
 
     int hitCounter = 0;
     for (int i = 0; i < total; i++) {
@@ -202,23 +211,6 @@ bool isXSymmetric(int total, int coords[][2], int edges[][10]) {
                 if ((coords[j][0] - center) != (center - coords[i][0])) {
                     return false;
                 }
-                //check edges
-                //first edge i has comp to first edge j has
-                //if the y coord of their edge is not equal
- /*               if (coords[edges[i][0]][1] != coords[edges[j][0]][1]) {
-                    return false;
-                }
-                //if the larger x coord - center != center - smallest x coord
-                if (coords[edges[i][0]][0] > coords[edges[j][0]][0]) {
-                    if((coords[edges[i][0]][0]- center) != (center - coords[edges[j][0]][0])) {
-                        return false;
-                    }
-                }
-                if (coords[edges[j][0]][0] > coords[edges[i][0]][0]) {
-                    if((coords[edges[j][0]][0]- center) != (center - coords[edges[i][0]][0])) {
-                        return false;
-                    }
-                }*/
             }
             //if there are no other vertex on the level and center is 0, center is the vert loc
             //if there are no other vertex on the level and center isnt 0, check that the vert is on center and if not return false
@@ -235,7 +227,7 @@ bool isXSymmetric(int total, int coords[][2], int edges[][10]) {
     return true;
 }
 
-bool isYSymmetric(int total, int coords[][2], int edges[][10]) {
+bool isYSymmetric(int total, int coords[][2]) {
 
     int hitCounter = 0;
     for (int i = 0; i < total; i++) {
@@ -263,23 +255,6 @@ bool isYSymmetric(int total, int coords[][2], int edges[][10]) {
                 if ((coords[j][1] - ycenter) != (ycenter - coords[i][1])) {
                     return false;
                 }
-                //check edges
-                //first edge i has comp to first edge j has
-                //if the y coord of their edge is not equal
-                /*               if (coords[edges[i][0]][1] != coords[edges[j][0]][1]) {
-                                   return false;
-                               }
-                               //if the larger x coord - center != center - smallest x coord
-                               if (coords[edges[i][0]][0] > coords[edges[j][0]][0]) {
-                                   if((coords[edges[i][0]][0]- center) != (center - coords[edges[j][0]][0])) {
-                                       return false;
-                                   }
-                               }
-                               if (coords[edges[j][0]][0] > coords[edges[i][0]][0]) {
-                                   if((coords[edges[j][0]][0]- center) != (center - coords[edges[i][0]][0])) {
-                                       return false;
-                                   }
-                               }*/
             }
             //if there are no other vertex on the level and center is 0, center is the vert loc
             //if there are no other vertex on the level and center isnt 0, check that the vert is on center and if not return false
@@ -302,9 +277,11 @@ void printXReflection() {
     for (int i = 0; i < totalVert; i++){
         if (vertLoc[i][0] < center){
             left += to_string(i);
+            left += " ";
         }
         else{
             reflection += to_string(i);
+            reflection += " ";
         }
     }
     cout << left << " <-- Left side of graph\n";
@@ -317,9 +294,11 @@ void printYReflection() {
     for (int i = 0; i < totalVert; i++){
         if (vertLoc[i][1] < ycenter){
             bottom += to_string(i);
+            bottom += " ";
         }
         else{
             reflection += to_string(i);
+            reflection += " ";
         }
     }
     cout << bottom << " <-- Bottom of graph\n";
@@ -328,68 +307,64 @@ void printYReflection() {
 
 int main()
 {
-
+    int checker = 0;
+    int counter= 0;
 
     std::cout << "How many pinned vertices are there?" << std::endl;
     cin >> numPinned;
     std::cout << "How many other vertices are there?" << std::endl;
     cin >> numVerticies;
-
-    //{{x coord, y coord, isValid}, {0,2,1}};
-//    vector<int> edges[numPinned+numVerticies + 1];
-    int checker = 0;
-    int counter, edgeCounter = 0;
+    if ((numPinned<0)||(numVerticies<0)) {
+        cout << "Input out of bounds\n";
+        return 0;
+    }
     totalVert = numPinned + numVerticies;
-    //to keep track of all verts the pins point to
-    //int vertLoc[totalVert][2];
-    //int vertEdges[totalVert][10];
-
     cout << "\n";
     Graph g1(totalVert, numPinned);
 
-    /*      for (int i = 0; i < totalVert; i++){
-              cout << "Enter the x coordinate of vertex " << i << "\n";
-              cin >> vertLoc[i][0];
-              cout << "Enter the y coordinate of vertex " << i << "\n";
-              cin >> vertLoc[i][1];
-          }*/
-    /*       for (int i = 0; i < totalVert; i++) {
-               edgeCounter = 0;
-              for (int j = 0; j < totalVert; j++) {
-                  if (i == j){
-                      continue;
-                  }
-                  if (j == 0){
-                      counter = 0;
-                  }
-                  cout << "Does vertex " << i << " have an edge in the direction of  vertex " << j << "? (enter 1 for yes, 0 for no)"<< "\n";
-                  cin >> checker;
-                  if(checker == 1){
-                          g1.addEdge(i, j);
-                          vertEdges[i][edgeCounter] = j;
-                          edgeCounter++;
-                          if (j < numPinned) {
-                              g1.addEdge(j, i);
-                          }
-                      checker = 0;
-                  }
-              }
-          }*/
-    int vertEdges[6][10] = {{1}, {4}, {5}, {2}, {0}, {3}};
+//    for (int i = 0; i < totalVert; i++){
+//        cout << "Enter the x coordinate of vertex " << i << "\n";
+//        cin >> vertLoc[i][0];
+//        cout << "Enter the y coordinate of vertex " << i << "\n";
+//        cin >> vertLoc[i][1];
+//    }
+//    for (int i = 0; i < totalVert; i++) {
+//        for (int j = 0; j < totalVert; j++) {
+//        if (i == j){
+//            continue;
+//        }
+//        if (j == 0){
+//            counter = 0;
+//        }
+//        cout << "Does vertex " << i << " have an edge in the direction of  vertex " << j << "? (enter 1 for yes, 0 for no)"<< "\n";
+//        cin >> checker;
+//        if ((checker < 0) || (checker > 1)){
+//             cout << "Invalid Answer\n";
+//             j--;
+//        }
+//        if(checker == 1){
+//            g1.addEdge(i, j);
+//            if (j < numPinned) {
+//                 g1.addEdge(j, i);
+//            }
+//            checker = 0;
+//            }
+//        }
+//    }
 
-    xsym = isXSymmetric(totalVert, vertLoc, vertEdges);
+    xsym = isXSymmetric(totalVert, vertLoc);
     cout << "\n\n Is it symmetric across a x axis? " << xsym << "\n";
     if (xsym){
         printXReflection();
     }
-    ysym = isYSymmetric(totalVert, vertLoc, vertEdges);
+    ysym = isYSymmetric(totalVert, vertLoc);
     cout << "\n\n Is it symmetric across a y axis? " << ysym << "\n\n";
     if (ysym){
         printYReflection();
     }
 
     //EX 1 X symmetry
-/*    g1.addEdge(2, 0);g1.addEdge(0, 2);g1.addEdge(2, 3);
+    g1.addEdge(2, 0);g1.addEdge(0, 2);g1.addEdge(2, 3);
     g1.addEdge(3, 4);g1.addEdge(4, 2);
 
     g1.addEdge(5, 1);g1.addEdge(1, 5);g1.addEdge(5, 6);
@@ -397,16 +372,27 @@ int main()
 
     g1.addEdge(8, 3);g1.addEdge(8, 9);
     g1.addEdge(9, 6);g1.addEdge(9, 8);
-*/
+
     //EX 2 Y symmetry
-    g1.addEdge(2, 0);g1.addEdge(0, 2);
-    g1.addEdge(2, 3);g1.addEdge(3, 4);g1.addEdge(4, 2);
+//    g1.addEdge(2, 0);g1.addEdge(0, 2);
+//    g1.addEdge(2, 3);g1.addEdge(3, 4);g1.addEdge(4, 2);
+//
+//    g1.addEdge(5, 1);g1.addEdge(1, 5);
+//    g1.addEdge(6, 7);g1.addEdge(7, 5);g1.addEdge(5, 6);
+//
+//    g1.addEdge(8, 6);g1.addEdge(8, 7);
+//    g1.addEdge(9, 3);g1.addEdge(9, 4);
 
-    g1.addEdge(5, 1);g1.addEdge(1, 5);
-    g1.addEdge(6, 7);g1.addEdge(7, 5);g1.addEdge(5, 6);
-
-    g1.addEdge(8, 6);g1.addEdge(8, 7);
-    g1.addEdge(9, 3);g1.addEdge(9, 4);
+    //EX 3
+//    g1.addEdge(4, 0);g1.addEdge(0, 4);
+//    g1.addEdge(5, 2);g1.addEdge(2, 5);
+//    g1.addEdge(9, 3);g1.addEdge(3, 9);
+//    g1.addEdge(8, 1);g1.addEdge(8, 10);
+//
+//    g1.addEdge(4, 6);g1.addEdge(5, 4);g1.addEdge(6, 5);
+//    g1.addEdge(7, 9);g1.addEdge(9, 8);g1.addEdge(8, 7);
+//
+//    g1.addEdge(12, 7);g1.addEdge(13, 7); g1.addEdge(10, 6);g1.addEdge(11, 6);
 
 
     g1.Assur();
